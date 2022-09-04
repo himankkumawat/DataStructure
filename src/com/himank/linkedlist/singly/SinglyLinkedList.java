f057325b1f4859b45b257c6e531a8931536de694
Himank <himankkumawat11@gmail.com>
2022-09-04 16:58:27 +0530

    singly linkedlist changes

--- src/com/himank/linkedlist/singly/SinglyLinkedList.java
+++ src/com/himank/linkedlist/singly/SinglyLinkedList.java
@@ -14,23 +14,46 @@ public class SinglyLinkedList {
         }
     }
 
-    public Node addNode(int data) {
+    public Node add(int data) {
         Node newNode = new Node(data);
-        if(head == null) {
+        if (head == null) {
             head = newNode;
             return newNode;
         }
         Node temp = head;
-        while(temp.next != null){
+        while (temp.next != null) {
             temp = temp.next;
         }
         temp.next = newNode;
         return newNode;
     }
 
+    public Node addAtIndex(int data, int index) {
+        Node newNode = new Node(data);
+        if (index == 0 && head != null) {
+            newNode.next = head;
+            head = newNode;
+            return newNode;
+        }
+        int i = 1;
+        Node temp = head;
+        while (index != i && temp.next != null) {
+            temp = temp.next;
+            i++;
+        }
+        if (i == index) {
+            newNode.next = temp.next;
+            temp.next = newNode;
+            return newNode;
+        }
+        System.out.println("Invalid Index...");
+        return null;
+
+    }
+
     public void displayLinkedList() {
         Node temp = head;
-        while(temp != null) {
+        while (temp != null) {
             System.out.println(temp.data);
             temp = temp.next;
         }
@@ -38,10 +61,11 @@ public class SinglyLinkedList {
 
     public static void main(String[] args) {
         SinglyLinkedList linkedList = new SinglyLinkedList();
-        linkedList.addNode(1);
-        linkedList.addNode(2);
-        linkedList.addNode(3);
-        linkedList.addNode(4);
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.addAtIndex(0, 1);
 
         linkedList.displayLinkedList();
     }
