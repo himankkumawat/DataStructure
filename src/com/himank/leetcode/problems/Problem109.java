1afe598197e270b4f3669f619edd0d7e298db595
Himank <himankkumawat11@gmail.com>
2023-03-12 00:07:47 +0530

    DS - BinarySearchTree (create BST from sorted LL)

---
+++ src/com/himank/leetcode/problems/Problem109.java
@@ -0,0 +1,87 @@
+package com.himank.leetcode.problems;
+
+public class Problem109 {
+
+    // https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/description/
+    public static void main(String[] args) {
+        Problem109 solution = new Problem109();
+        ListNode head = solution.getListNode();
+        TreeNode root = solution.sortedListToBST(head);
+        System.out.println(root.val);
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        int size = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            size++;
+        }
+        int[] arr = new int[size];
+        temp = head;
+        int index = 0;
+        while (temp != null) {
+            arr[index++] = temp.val;
+            temp = temp.next;
+        }
+        return insert(null, arr, 0, size);
+    }
+
+    private TreeNode insert(TreeNode root, int[] arr, int start, int end) {
+        if (end < 0 || start >= arr.length || start > end) return null;
+        int mid = start + (end - start) / 2;
+        root = new TreeNode(arr[mid]);
+        root.left = insert(root.left, arr, start, mid - 1);
+        root.right = insert(root.right, arr, mid + 1, end);
+        return root;
+    }
+
+    private ListNode getListNode() {
+        ListNode head = new ListNode(-10);
+        ListNode temp = head;
+        temp.next = new ListNode(-3);
+        temp = temp.next;
+        temp.next = new ListNode(0);
+        temp = temp.next;
+        temp.next = new ListNode(5);
+        temp = temp.next;
+        temp.next = new ListNode(9);
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
