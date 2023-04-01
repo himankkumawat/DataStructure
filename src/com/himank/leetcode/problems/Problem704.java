117c2ebbd165704fbce840888e516656154a2de3
Himank <himankkumawat11@gmail.com>
2023-04-01 15:30:53 +0530

    DS- Leetcode Problem704 - Binary Search

---
+++ src/com/himank/leetcode/problems/Problem704.java
@@ -0,0 +1,23 @@
+package com.himank.leetcode.problems;
+
+public class Problem704 {
+
+    // https://leetcode.com/problems/binary-search/
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println("Index of target is : " + search(nums, target));
+    }
+
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target == nums[mid]) return mid;
+            if (target > nums[mid]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
