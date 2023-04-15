137eee513ce7321676f7d50b25d42266db01542d
Himank <himankkumawat11@gmail.com>
2023-04-15 23:59:19 +0530

    DS - Leetcode Problem33 - Search in Rotated Sorted Array - formatting

--- src/com/himank/leetcode/problems/Problem33.java
+++ src/com/himank/leetcode/problems/Problem33.java
@@ -11,27 +11,25 @@ public class Problem33 {
     }
 
     public static int search(int[] nums, int target) {
-        int low=0;
-        int high=nums.length-1;
+        int low = 0;
+        int high = nums.length - 1;
 
-        while(low<=high){
-            int mid= (low + high) /2;
-            if(nums[mid]==target) return mid;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
 
-            if(nums[low]<=nums[mid]){
+            if (nums[low] <= nums[mid]) {
 
-                if(target<=nums[mid] && target>=nums[low]){
-                    high=mid-1;
-                }else{
-                    low=mid+1;
+                if (target <= nums[mid] && target >= nums[low]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }
-            }
-
-            else{
-                if(target>=nums[mid] && target<=nums[high]){
-                    low=mid+1;
-                }else{
-                    high=mid-1;
+            } else {
+                if (target >= nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
                 }
             }
         }
