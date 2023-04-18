972d32ec349a469d8dd60a2a17ad074cd7612d7f
Himank <himankkumawat11@gmail.com>
2023-04-19 00:46:59 +0530

    DS - DP - Knapsack SubSetProblem

--- src/com/himank/dynamic_programming/knapsack/SubSetProblem.java
+++ src/com/himank/dynamic_programming/knapsack/SubSetProblem.java
@@ -4,8 +4,10 @@ public class SubSetProblem {
 
     public static void main(String[] args) {
         int[] arr = {2, 3, 7, 8, 10};
-        int targetSum = 14;
-        System.out.println("Target sum is possible? : " + isSubSetSumExist(arr, targetSum));
+        int targetSum = 15;
+        System.out.println("Target sum is possible via Top down ? : " + isSubSetSumExist(arr, targetSum));
+        System.out.println("Target sum is possible via Memoization ? : " + isSubsetSumExistsRecursivelyMemoization(arr, targetSum, arr.length));
+        System.out.println("Target sum is possible via Recursive ? : " + isSubsetSumExistsRecursively(arr, targetSum, arr.length));
     }
 
     public static boolean isSubSetSumExist(int[] arr, int targetSum) {
@@ -31,5 +33,36 @@ public class SubSetProblem {
         }
         return t[n][targetSum];
 
+    }
+
+    static Boolean[][] dp = new Boolean[5 + 1][15 + 1];                         // This is hardcoded for the example taken above.
+
+    static {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0) dp[i][j] = false;
+                if (j == 0) dp[i][j] = true;
+                else dp[i][j] = null;
+            }
+        }
+    }
+
+    public static boolean isSubsetSumExistsRecursivelyMemoization(int[] arr, int targetSum, int n) {
+        if (targetSum == 0) return true;
+        if (n == 0) return false;
+        if (dp[n][targetSum] != null) return dp[n][targetSum];
+        if (arr[n - 1] > targetSum)
+            return dp[n][targetSum] = isSubsetSumExistsRecursivelyMemoization(arr, targetSum, n - 1);
+        return dp[n][targetSum] = isSubsetSumExistsRecursivelyMemoization(arr, targetSum - arr[n - 1], n - 1) ||
+                isSubsetSumExistsRecursivelyMemoization(arr, targetSum, n - 1);
+    }
+
+    public static boolean isSubsetSumExistsRecursively(int[] arr, int targetSum, int n) {
+        if (targetSum == 0) return true;
+        if (n == 0) return false;
+        if (arr[n - 1] > targetSum)
+            return isSubsetSumExistsRecursively(arr, targetSum, n - 1);
+        return isSubsetSumExistsRecursively(arr, targetSum - arr[n - 1], n - 1) ||
+                isSubsetSumExistsRecursively(arr, targetSum, n - 1);
     }
 }
