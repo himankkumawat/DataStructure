bd59944cd0f3948d4b73959a585a00f7d3556194
Himank <himankkumawat11@gmail.com>
2023-05-24 21:32:58 +0530

    DS - Leetcode - Problem2542 - Maximum subsequence score

---
+++ src/com/himank/leetcode/problems/Problem2542.java
@@ -0,0 +1,42 @@
+package com.himank.leetcode.problems;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Problem2542 {
+
+    // https://leetcode.com/problems/maximum-subsequence-score/
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3, 3, 2}, nums2 = {2, 1, 3, 4};
+        int k = 3;
+
+        System.out.println("Max subsequence score: " + maxScore(nums1, nums2, k));
+    }
+
+    public static long maxScore(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        int[][] pairs = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            pairs[i] = new int[]{nums1[i], nums2[i]};
+        }
+        Arrays.sort(pairs, (a, b) -> b[1] - a[1]);
+
+        PriorityQueue<Integer> topKHeap = new PriorityQueue<>(k, (a, b) -> a - b);
+        long topKSum = 0;
+        for (int i = 0; i < k; ++i) {
+            topKSum += pairs[i][0];
+            topKHeap.add(pairs[i][0]);
+        }
+
+        long answer = topKSum * pairs[k - 1][1];
+
+        for (int i = k; i < n; ++i) {
+            topKSum += pairs[i][0] - topKHeap.poll();
+            topKHeap.add(pairs[i][0]);
+
+            answer = Math.max(answer, topKSum * pairs[i][1]);
+        }
+
+        return answer;
+    }
+}
