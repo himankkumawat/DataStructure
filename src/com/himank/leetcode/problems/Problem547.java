546ff357894c600790ed1d5d690415f61ece7da4
Himank <himankkumawat11@gmail.com>
2023-06-04 21:13:48 +0530

    DS - Leetcode - Problem547 - number of province

---
+++ src/com/himank/leetcode/problems/Problem547.java
@@ -0,0 +1,33 @@
+package com.himank.leetcode.problems;
+
+public class Problem547 {
+
+    public static void main(String[] args) {
+        int[][] isConnected = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        System.out.println("Total number of province : " + findCircleNum(isConnected));
+    }
+
+    public static void dfs(int node, int[][] isConnected, boolean[] visit) {
+        visit[node] = true;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (isConnected[node][i] == 1 && !visit[i]) {
+                dfs(i, isConnected, visit);
+            }
+        }
+    }
+
+    public static int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int numberOfComponents = 0;
+        boolean[] visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                numberOfComponents++;
+                dfs(i, isConnected, visit);
+            }
+        }
+
+        return numberOfComponents;
+    }
+}
