6d52ed93c1194fb1e4f62273b42a73a52a03221f
Himank <himankkumawat11@gmail.com>
2023-06-16 21:49:00 +0530

    DS - Leetcode - Problem100 - trees are same or not

---
+++ src/com/himank/leetcode/problems/Problem100.java
@@ -0,0 +1,34 @@
+package com.himank.leetcode.problems;
+
+public class Problem100 {
+
+    // https://leetcode.com/problems/same-tree/
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(3);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(3);
+
+        System.out.println("Trees are same ? " + isSameTree(root1, root2));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
