44dd8008a924b17fc8cc0ec69e2cb76e47f72528
Himank <himankkumawat11@gmail.com>
2023-06-18 16:24:21 +0530

    DS - Leetcode - Problem104 - Maximum depth of binary tree

---
+++ src/com/himank/leetcode/problems/Problem104.java
@@ -0,0 +1,30 @@
+package com.himank.leetcode.problems;
+
+public class Problem104 {
+
+    // https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("Maximum depth = " + maxDepth(root));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
