82cbdc0fc2a16683a0b3a661758026c9852535e9
Himank <himankkumawat11@gmail.com>
2023-07-10 23:33:28 +0530

    DS - Leetcode - Problem111 - Minimum depth of a binary tree

---
+++ src/com/himank/leetcode/problems/Problem111.java
@@ -0,0 +1,33 @@
+package com.himank.leetcode.problems;
+
+public class Problem111 {
+
+    // https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("Minimum depth is : " + minDepth(root));
+    }
+
+    public static int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        if (root.left == null) return minDepth(root.right) + 1;
+        if (root.right == null) return minDepth(root.left) + 1;
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
