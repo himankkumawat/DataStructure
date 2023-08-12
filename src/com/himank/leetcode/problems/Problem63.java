b3f266f5a304ba1348710fc761c11876be772f24
Himank <himankkumawat11@gmail.com>
2023-08-12 11:49:11 +0530

    DS - Leetcode - Problem63 - Unique path

---
+++ src/com/himank/leetcode/problems/Problem63.java
@@ -0,0 +1,42 @@
+package com.himank.leetcode.problems;
+
+public class Problem63 {
+
+    // https://leetcode.com/problems/unique-paths-ii/description/
+    public static void main(String[] args) {
+        int[][] obstacleGrid = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        System.out.println("Unique path : " + uniquePathsWithObstacles(obstacleGrid));
+    }
+
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+        int R = obstacleGrid.length;
+        int C = obstacleGrid[0].length;
+
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        obstacleGrid[0][0] = 1;
+
+        for (int i = 1; i < R; i++) {
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
+        }
+
+        for (int i = 1; i < C; i++) {
+            obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) ? 1 : 0;
+        }
+
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                } else {
+                    obstacleGrid[i][j] = 0;
+                }
+            }
+        }
+
+        return obstacleGrid[R - 1][C - 1];
+    }
+}
