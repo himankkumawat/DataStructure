5a2cd36560bb52e887e665635a85540af33df4fc
Himank <himankkumawat11@gmail.com>
2023-08-25 21:18:17 +0530

    DS - Leetcode - Problem97 - Is interleaving string

---
+++ src/com/himank/leetcode/problems/Problem97.java
@@ -0,0 +1,31 @@
+package com.himank.leetcode.problems;
+
+public class Problem97 {
+
+    // https://leetcode.com/problems/interleaving-string/description/
+    public static void main(String[] args) {
+        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc";
+        System.out.println("Is interleaving string: " + isInterleave(s1, s2, s3));
+    }
+
+    public static boolean isInterleave(String s1, String s2, String s3) {
+        if (s3.length() != s1.length() + s2.length()) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                } else {
+                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+}
