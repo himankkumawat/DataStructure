003721bd787551a980a21a0f191f8d34dfd331fb
Himank <himankkumawat11@gmail.com>
2023-09-04 21:57:15 +0530

    DS - Leetcode - Problem141 - cyclic linkedlist

---
+++ src/com/himank/leetcode/problems/Problem141.java
@@ -0,0 +1,40 @@
+package com.himank.leetcode.problems;
+
+public class Problem141 {
+
+    // https://leetcode.com/problems/linked-list-cycle/
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        System.out.println("Cycle exist? : " + hasCycle(head));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
