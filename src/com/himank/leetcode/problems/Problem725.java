8561a38074e1046010388d3d309c3c335b664cba
Himank <himankkumawat11@gmail.com>
2023-09-06 22:09:50 +0530

    DS - Leetcode - Problem725 - split list into parts

---
+++ src/com/himank/leetcode/problems/Problem725.java
@@ -0,0 +1,77 @@
+package com.himank.leetcode.problems;
+
+public class Problem725 {
+
+    // https://leetcode.com/problems/split-linked-list-in-parts/
+    public static void main(String[] args) {
+        ListNode head = getListNode();
+        ListNode[] result = splitListToParts(head, 3);
+        for(ListNode node : result) {
+            ListNode temp = node;
+            while (temp != null) {
+                System.out.print(temp.val + "   ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+    private static ListNode getListNode() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(8);
+        head.next.next.next.next.next.next.next.next = new ListNode(9);
+        head.next.next.next.next.next.next.next.next.next = new ListNode(10);
+
+        return head;
+    }
+
+    public static ListNode[] splitListToParts(ListNode root, int k) {
+        ListNode cur = root;
+        int N = 0;
+        while (cur != null) {
+            cur = cur.next;
+            N++;
+        }
+
+        int width = N / k, rem = N % k;
+
+        ListNode[] ans = new ListNode[k];
+        cur = root;
+        for (int i = 0; i < k; ++i) {
+            ListNode head = cur;
+            for (int j = 0; j < width + (i < rem ? 1 : 0) - 1; ++j) {
+                if (cur != null) cur = cur.next;
+            }
+            if (cur != null) {
+                ListNode prev = cur;
+                cur = cur.next;
+                prev.next = null;
+            }
+            ans[i] = head;
+        }
+        return ans;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
