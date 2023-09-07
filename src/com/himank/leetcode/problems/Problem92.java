a2331409206773cbaf70f7e17f16b34d965056af
Himank <himankkumawat11@gmail.com>
2023-09-07 21:48:33 +0530

    DS - Leetcode - Problem92 - reverse linkedlist

---
+++ src/com/himank/leetcode/problems/Problem92.java
@@ -0,0 +1,60 @@
+package com.himank.leetcode.problems;
+
+import java.util.List;
+
+public class Problem92 {
+
+    // https://leetcode.com/problems/reverse-linked-list-ii/
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode result = reverseBetween(head, 2, 4);
+        while(null != result) {
+            System.out.print(result.val + "  ");
+            result = result.next;
+        }
+    }
+
+    public static ListNode reverseBetween(ListNode head, int m, int n) {
+
+        if (head == null) {
+            return null;
+        }
+        ListNode cur = head, prev = null;
+        while (m > 1) {
+            prev = cur;
+            cur = cur.next;
+            m--;
+            n--;
+        }
+        ListNode con = prev, tail = cur;
+        ListNode third = null;
+        while (n > 0) {
+            third = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = third;
+            n--;
+        }
+        if (con != null) {
+            con.next = prev;
+        } else {
+            head = prev;
+        }
+
+        tail.next = cur;
+        return head;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
