462406f84f4dd2ceacfe01cf831ae50586817d0d
Himank <himankkumawat11@gmail.com>
2023-09-13 21:09:05 +0530

    DS - Leetcode - Problem135 - Minimum candies to distribute

---
+++ src/com/himank/leetcode/problems/Problem135.java
@@ -0,0 +1,34 @@
+package com.himank.leetcode.problems;
+
+import java.util.Arrays;
+
+public class Problem135 {
+
+    // https://leetcode.com/problems/candy/
+    public static void main(String[] args) {
+        int[] ratings = {1, 0, 2};
+        System.out.println("Minimum candies to distribute " + candy(ratings));
+    }
+
+    public static int candy(int[] ratings) {
+        int sum = 0;
+        int[] left2right = new int[ratings.length];
+        int[] right2left = new int[ratings.length];
+        Arrays.fill(left2right, 1);
+        Arrays.fill(right2left, 1);
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left2right[i] = left2right[i - 1] + 1;
+            }
+        }
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                right2left[i] = right2left[i + 1] + 1;
+            }
+        }
+        for (int i = 0; i < ratings.length; i++) {
+            sum += Math.max(left2right[i], right2left[i]);
+        }
+        return sum;
+    }
+}
