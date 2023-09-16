1346a9628af9862bff9e4fa71dba608286ecb107
Himank <himankkumawat11@gmail.com>
2023-09-16 14:55:14 +0530

    DS - Leetcode - Problem1631 - Minimum efforts required

---
+++ src/com/himank/leetcode/problems/Problem1631.java
@@ -0,0 +1,64 @@
+package com.himank.leetcode.problems;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Problem1631 {
+
+    // https://leetcode.com/problems/path-with-minimum-effort/
+    public static void main(String[] args) {
+        int[][] heights = {{1, 2, 2}, {3, 8, 2}, {5, 3, 5}};
+        System.out.println("Minimum efforts required is : " + minimumEffortPath(heights));
+    }
+
+    private static int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public static int minimumEffortPath(int[][] heights) {
+        int row = heights.length;
+        int col = heights[0].length;
+        int[][] differenceMatrix = new int[row][col];
+        for (int[] eachRow : differenceMatrix)
+            Arrays.fill(eachRow, Integer.MAX_VALUE);
+        differenceMatrix[0][0] = 0;
+        PriorityQueue<Cell> queue = new PriorityQueue<>(Comparator.comparing(a -> a.difference));
+        boolean[][] visited = new boolean[row][col];
+        queue.add(new Cell(0, 0, differenceMatrix[0][0]));
+
+        while (!queue.isEmpty()) {
+            Cell curr = queue.poll();
+            visited[curr.x][curr.y] = true;
+            if (curr.x == row - 1 && curr.y == col - 1)
+                return curr.difference;
+            for (int[] direction : directions) {
+                int adjacentX = curr.x + direction[0];
+                int adjacentY = curr.y + direction[1];
+                if (isValidCell(adjacentX, adjacentY, row, col) && !visited[adjacentX][adjacentY]) {
+                    int currentDifference = Math.abs(heights[adjacentX][adjacentY] - heights[curr.x][curr.y]);
+                    int maxDifference = Math.max(currentDifference, differenceMatrix[curr.x][curr.y]);
+                    if (differenceMatrix[adjacentX][adjacentY] > maxDifference) {
+                        differenceMatrix[adjacentX][adjacentY] = maxDifference;
+                        queue.add(new Cell(adjacentX, adjacentY, maxDifference));
+                    }
+                }
+            }
+        }
+        return differenceMatrix[row - 1][col - 1];
+    }
+
+    private static boolean isValidCell(int x, int y, int row, int col) {
+        return x >= 0 && x <= row - 1 && y >= 0 && y <= col - 1;
+    }
+
+    private static class Cell {
+        int x;
+        int y;
+        Integer difference;
+
+        Cell(int x, int y, Integer difference) {
+            this.x = x;
+            this.y = y;
+            this.difference = difference;
+        }
+    }
+}
