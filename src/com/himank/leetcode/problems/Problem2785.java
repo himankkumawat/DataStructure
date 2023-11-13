b9b98dd07a9996a7f9aa96597b4c6a6345c285b5
Himank <himankkumawat11@gmail.com>
2023-11-13 13:06:59 +0530

    DS - Leetcode - Problem2785 - Sort vowel in a string

---
+++ src/com/himank/leetcode/problems/Problem2785.java
@@ -0,0 +1,46 @@
+package com.himank.leetcode.problems;
+
+public class Problem2785 {
+
+    // https://leetcode.com/problems/sort-vowels-in-a-string/
+    public static void main(String[] args) {
+        String str = "lEetcOde";
+
+        System.out.println("Result string is: " + sortVowels(str));
+    }
+
+    private static boolean isVowel(Character c) {
+        return c == 'a' || c == 'e' || c == 'o'|| c == 'u'|| c == 'i'
+                || c == 'A' || c == 'E' || c == 'O'|| c == 'U'|| c == 'I';
+    }
+
+    public static String sortVowels(String s) {
+        int[] count = new int[1000];
+
+        // Store the frequencies for each character.
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                count[c - 'A']++;
+            }
+        }
+
+        // Sorted string having all the vowels.
+        String sortedVowel = "AEIOUaeiou";
+        StringBuilder ans = new StringBuilder();
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!isVowel(s.charAt(i))) {
+                ans.append(s.charAt(i));
+            } else {
+                // Skip to the character which is having remaining count.
+                while (count[sortedVowel.charAt(j) - 'A'] == 0) {
+                    j++;
+                }
+
+                ans.append(sortedVowel.charAt(j));
+                count[sortedVowel.charAt(j) - 'A']--;
+            }
+        }
+        return ans.toString();
+    }
+}
