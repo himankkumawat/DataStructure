02ac69c83a1bdbe59735126a0cf5851db09aa648
Himank <himankkumawat11@gmail.com>
2023-12-07 20:55:24 +0530

    DS - Leetcode - Problem1903 - Largest Odd Number in String

---
+++ src/com/himank/leetcode/problems/Problem1903.java
@@ -0,0 +1,18 @@
+package com.himank.leetcode.problems;
+
+public class Problem1903 {
+
+    // https://leetcode.com/problems/largest-odd-number-in-string/
+    public static void main(String[] args) {
+        String number = "35427";
+    }
+
+    public static String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if (Character.getNumericValue(num.charAt(i)) % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+}
