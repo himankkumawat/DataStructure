e1464a1a54c373371bf17a1a84fe9522cd0015e5
Himank <himankkumawat11@gmail.com>
2023-12-08 22:34:03 +0530

    DS - Leetcode - Problem606 - String from binary tree

---
+++ src/com/himank/leetcode/problems/Problem606.java
@@ -0,0 +1,47 @@
+package com.himank.leetcode.problems;
+
+public class Problem606 {
+
+    // https://leetcode.com/problems/construct-string-from-binary-tree/
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+
+        System.out.println("String binary string: " + tree2str(root));
+    }
+
+    public static String tree2str(TreeNode t) {
+        StringBuilder res = new StringBuilder();
+        dfs(t, res);
+        return res.toString();
+    }
+
+    public static void dfs(TreeNode t, StringBuilder res) {
+        if (t == null)
+            return;
+        res.append(t.val);
+        if (t.left == null && t.right == null)
+            return;
+        res.append('(');
+        dfs(t.left, res);
+        res.append(')');
+        if (t.right != null) {
+            res.append('(');
+            dfs(t.right, res);
+            res.append(')');
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+    }
+}
