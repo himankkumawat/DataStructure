6a6dc1718db6414818e9d20b0ae27299fe5958c7
Himank <himankkumawat11@gmail.com>
2023-12-09 22:04:32 +0530

    DS - Leetcode - Problem230 - Kth Smallest Element in a BST

---
+++ src/com/himank/leetcode/problems/Problem230.java
@@ -0,0 +1,40 @@
+package com.himank.leetcode.problems;
+
+import java.util.LinkedList;
+
+public class Problem230 {
+
+    // https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.left.left = new TreeNode(2);
+
+        System.out.println("Smallest element is : " + kthSmallest(root, 1));
+    }
+
+    public static int kthSmallest(TreeNode root, int k) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+
+        while (true) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (--k == 0) return root.val;
+            root = root.right;
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
