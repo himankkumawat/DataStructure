cf022d3adbf08992898eff82cbfbacf2b024ffdd
Himank <himankkumawat11@gmail.com>
2023-12-17 10:28:24 +0530

    DS - Leetcode - Problem296 - Best meeting point

---
+++ src/com/himank/leetcode/problems/Problem296.java
@@ -0,0 +1,54 @@
+package com.himank.leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem296 {
+
+    public static void main(String[] args) {
+        int[][] grid = {{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}};
+        System.out.println("Best meeting point: " + minTotalDistance(grid));
+    }
+
+    public static int minTotalDistance(int[][] grid) {
+        List<Integer> rows = collectRows(grid);
+        List<Integer> cols = collectCols(grid);
+        return minDistance1D(rows) + minDistance1D(cols);
+    }
+
+    private static int minDistance1D(List<Integer> points) {
+        int distance = 0;
+        int i = 0;
+        int j = points.size() - 1;
+        while (i < j) {
+            distance += points.get(j) - points.get(i);
+            i++;
+            j--;
+        }
+        return distance;
+    }
+
+    private static List<Integer> collectRows(int[][] grid) {
+        List<Integer> rows = new ArrayList<>();
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == 1) {
+                    rows.add(row);
+                }
+            }
+        }
+        return rows;
+    }
+
+    private static List<Integer> collectCols(int[][] grid) {
+        List<Integer> cols = new ArrayList<>();
+        for (int col = 0; col < grid[0].length; col++) {
+            for (int[] ints : grid) {
+                if (ints[col] == 1) {
+                    cols.add(col);
+                }
+            }
+        }
+        return cols;
+    }
+}
