3eb0c2adf7d0fac9c0c405805e89d4bf4dab8e9d
Himank <himankkumawat11@gmail.com>
2023-12-25 13:59:45 +0530

    DS - Leetcode - Problem91 - Decode ways

---
+++ src/com/himank/leetcode/problems/Problem91.java
@@ -0,0 +1,36 @@
+package com.himank.leetcode.problems;
+
+public class Problem91 {
+
+    // https://leetcode.com/problems/decode-ways/
+    public static void main(String[] args) {
+        String s = "226";
+        System.out.println("Decode ways: " + numDecodings(s));
+    }
+
+    public static int numDecodings(String s) {
+        final int n = s.length();
+        // dp[i] := the number of ways to decode s[i..n)
+        int[] dp = new int[n + 1];
+        dp[n] = 1; // ""
+        dp[n - 1] = isValid(s.charAt(n - 1)) ? 1 : 0;
+
+        for (int i = n - 2; i >= 0; --i) {
+            if (isValid(s.charAt(i)))
+                dp[i] += dp[i + 1];
+            if (isValid(s.charAt(i), s.charAt(i + 1)))
+                dp[i] += dp[i + 2];
+        }
+
+        return dp[0];
+    }
+
+    private static boolean isValid(char c) {
+        return c != '0';
+    }
+
+    private static boolean isValid(char c1, char c2) {
+        return c1 == '1' || c1 == '2' && c2 < '7';
+    }
+
+}
